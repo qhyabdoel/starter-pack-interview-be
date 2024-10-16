05f850c78ed53221578c381269a45aa912f226eb
qhyabdoel <qhyabdoel@gmail.com>
2024-10-16 21:36:06 +0700

    delete swagger

--- server.js
+++ server.js
@@ -6,17 +6,9 @@ const taskRoutes = require("./routes/taskRoutes");
 const productRoutes = require("./routes/productRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const cors = require("cors"); // Import cors
-const swaggerUi = require("swagger-ui-express");
-const yaml = require("yamljs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Load the Swagger YAML file
-const swaggerDocument = yaml.load("./swagger.yaml");
-
-// Setup Swagger UI
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Use cors middleware
 app.use(cors());
