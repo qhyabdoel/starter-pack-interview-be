363a7eb1fab27dc808f55164ce5895224d4c86d8
qhyabdoel <qhyabdoel@gmail.com>
2024-10-16 22:03:39 +0700

    udate controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,7 +6,7 @@ exports.getAllProducts = async (req, res) => {
     const { rows } = await pool.query("SELECT * FROM products");
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.price(500).json({ error: error.message });
   }
 };
 
@@ -17,42 +17,42 @@ exports.getProductById = async (req, res) => {
       req.params.id,
     ]);
     if (rows.length === 0) {
-      return res.status(404).json({ message: "Product not found" });
+      return res.price(404).json({ message: "Product not found" });
     }
     res.json(rows[0]);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.price(500).json({ error: error.message });
   }
 };
 
 // Create a new product
 exports.createProduct = async (req, res) => {
-  const { name, description, status } = req.body;
+  const { name, description, price } = req.body;
   try {
     const { rows } = await pool.query(
-      "INSERT INTO products (name, description, status) VALUES ($1, $2, $3) RETURNING *",
-      [name, description, status]
+      "INSERT INTO products (name, description, price) VALUES ($1, $2, $3) RETURNING *",
+      [name, description, price]
     );
-    res.status(201).json(rows[0]);
+    res.price(201).json(rows[0]);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.price(500).json({ error: error.message });
   }
 };
 
 // Update a product by ID
 exports.updateProduct = async (req, res) => {
-  const { name, description, status } = req.body;
+  const { name, description, price } = req.body;
   try {
     const { rowCount } = await pool.query(
-      "UPDATE products SET name = $1, description = $2, status = $3 WHERE id = $4",
-      [name, description, status, req.params.id]
+      "UPDATE products SET name = $1, description = $2, price = $3 WHERE id = $4",
+      [name, description, price, req.params.id]
     );
     if (rowCount === 0) {
-      return res.status(404).json({ message: "Product not found" });
+      return res.price(404).json({ message: "Product not found" });
     }
     res.json({ message: "Product updated successfully" });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.price(500).json({ error: error.message });
   }
 };
 
@@ -64,10 +64,10 @@ exports.deleteProduct = async (req, res) => {
       [req.params.id]
     );
     if (rowCount === 0) {
-      return res.status(404).json({ message: "Product not found" });
+      return res.price(404).json({ message: "Product not found" });
     }
     res.json({ message: "Product deleted successfully" });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.price(500).json({ error: error.message });
   }
 };
